bf62af0e31b1a8ab0c24dcb2bd55ed8bd402e670
Nityahiray <nityahiray20@gmail.com>
2026-07-20 12:26:30 +0530

    Changes in EvenDigits code

--- Linearsearch/EvenDigits.java
+++ Linearsearch/EvenDigits.java
@@ -4,7 +4,8 @@ public class EvenDigits {
     public static void main(String[] args) {
         int[] nums = { 12, 345, 2, 6, 7896 };
 
-        System.out.println(findNumbers(nums));
+        // System.out.println(findNumbers(nums));
+        System.out.println(digits2(345678));
     }
 
     static int findNumbers(int[] nums) {
@@ -23,6 +24,10 @@ public class EvenDigits {
     static boolean even(int num) {
         int numberOfDigits = digits(num);
         return numberOfDigits % 2 == 0;
+    }
+
+    static int digits2(int num) {
+        return (int) (Math.log10(num));
     }
 
     // Function to count the number of digits
