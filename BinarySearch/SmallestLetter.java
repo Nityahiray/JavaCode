381f9fcfcc051662cac5669d578d08442d963176
Nityahiray <nityahiray20@gmail.com>
2026-07-24 11:00:34 +0530

    Leetcode Q.no 744

---
+++ BinarySearch/SmallestLetter.java
@@ -0,0 +1,30 @@
+//744.Find smallest Greater Than Target
+
+package BinarySearch;
+
+public class SmallestLetter {
+    public static void main(String[] args) {
+
+    }
+
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+
+        while (start <= end) {
+
+            // Find the middle element
+            // This formula prevents integer overflow
+            int mid = start + (end - start) / 2;
+
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else if (target > letters[mid]) {
+                start = mid + 1;
+            }
+        }
+
+        return letters[start % letters.length];
+    }
+
+}
